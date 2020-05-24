742588a7620803d2107693fa34f1d38adc114c61
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-05-24 10:45:10 +0000

    has made some changes to the javascript for mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,12 @@
-
+$(document).ready(function () {
           /*-------------------------------------------------------------------------------
     	  Mobile Navigation
     	-------------------------------------------------------------------------------*/
-
-    $('li.has-child > a').after('<div class="child-trigger"><i></i></div>');
 
     $('#nav-toggle-label, .mobile-trigger').on('click', function () {
         $('body').toggleClass('mobile-open');
         $('.child-trigger').removeClass('child-open');
         $('.mobile-menu .sub-menu').slideUp(250);
-        return false;
-    });
-
-    $('.mobile-trigger2').on('click', function () {
-        $('body').toggleClass('mobile-open-2');
-        $('.child-trigger').removeClass('child-open');
-        $('.mobile-menu .sub-menu').slideUp(250);
-        return false;
-    });
-
-    $('.child-trigger').on('click', function () {
-        var $parent = $(this).parent().siblings('.has-child');
-        $parent.find('.child-trigger').removeClass('child-open');
-        $parent.find('.sub-menu').slideUp(250);
-
-        $(this).next('.sub-menu').slideToggle(250);
-        $(this).toggleClass('child-open');
-        return false;
-    });
-
-    $('.mob-menu-overlay').on('click', function () {
-        $('body').removeClass('mobile-open');
-        $('body').removeClass('mobile-open-2');
-        $('.child-trigger').removeClass('child-open');
-        $('.mob-menu .sub-menu').slideUp(250);
         return false;
     });
 
@@ -54,4 +27,7 @@
         $('.wd-menu pop-scroll li a').click(function () {
         $('.wd-menu pop-scroll li a').removeClass("active_mobile");
         $(this).addClass("active_mobile");
-    });   
+    });  
+    
+
+   }); 

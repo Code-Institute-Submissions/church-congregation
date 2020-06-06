d6f88b9201237b936ecfbd670b13373649ff605f
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-06-06 21:59:19 +0000

    have built data-countdown javascripte for events, to set date we're counting down to the events date

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,3 +61,36 @@ $(document).ready(function () {
          
 
    }); 
+
+     //***---------------------data-countdown for events------------------->
+// Set the date we're counting down to
+$(document).ready(function(){
+var countDownDate = new Date("Sep 5, 2020 10:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="data-countd"
+  document.getElementById("data-countd").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("data-countdown").innerHTML = "THER IS NO EVENT";
+  }
+}, 1000);
+});
+//***---------------------data-countdown for events-------------------***//
